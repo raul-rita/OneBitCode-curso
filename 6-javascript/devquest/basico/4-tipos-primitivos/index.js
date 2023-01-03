f47cafab5ad4e28a5a9693a2f9d19b88ac59c5d0
Raul Rita <raul.vinicius@live.com>
2023-01-03 20:20:10 -0300

    tipos primitivos de variaveis

---
+++ 6-javascript/devquest/basico/4-tipos-primitivos/index.js
@@ -0,0 +1,10 @@
+let nome = 'Raul' //string - tipo texto definido por aspas
+
+let idade = 30 // number - recebe um valor numeral, sendo positivos ou negativos, inteiros ou decimais
+
+let usuarioVerificado = true // booleano, define uma variável como true ou false, usado principalmente 
+//para validacoes
+
+let pessoas = null // tipo inválido, sem valor, não ocupa nenhum espaço
+
+let pessoaa = undefined // quando a variável está indefinida
