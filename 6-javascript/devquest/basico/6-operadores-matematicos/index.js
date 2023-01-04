ac76fb54f60bc81a94a79d7081f484896c346d90
Raul Rita <raul.vinicius@live.com>
2023-01-04 16:49:56 -0300

    operadores matematicos e lacos

---
+++ 6-javascript/devquest/basico/6-operadores-matematicos/index.js
@@ -0,0 +1,11 @@
+const primeiroValor = 10
+const segundoValor = 5
+
+console.log("Soma =", primeiroValor + segundoValor)
+console.log("Subtração =", primeiroValor - segundoValor)
+console.log("Divisão =", primeiroValor / segundoValor)
+console.log("Multiplicação =", primeiroValor * segundoValor)
+console.log("Resto da divisão =", primeiroValor % segundoValor)
+
+/* Quando usa o operador de soma com tipos de variáveis diferentes 
+ao invés de somar os valor é feito a CONCATENAÇÃO */
---
+++ 6-javascript/devquest/basico/7-lacos/index.js
@@ -0,0 +1,24 @@
+/* Sintaxe do FOR
+    for(inicializador; condicao; incremento) {
+        declaracao
+    }
+*/
+
+for(let i = 0; i < 5 ; i++) {
+    console.log(i)
+}
+
+/*
+    Sintaxe do WHILE
+    while(condicao) {
+        bloco de execucao
+    }
+*/
+
+    let a = 10
+    let b = 15
+
+    while(a < b) {
+        console.log("Incrementando a variável A = " + a)
+        a++
+    }
