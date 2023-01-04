721919fe0154f9d81a35dfa481910b6eb6aeeff8
Raul Rita <raul.vinicius@live.com>
2023-01-04 20:07:50 -0300

    funcoes e fim do modulo de JS basico

---
+++ 6-javascript/devquest/basico/9-funcoes/index.js
@@ -0,0 +1,29 @@
+/* Sintaxe da função
+
+    function nomeDaFuncao () {
+        execucao da funcao
+    }
+
+    nomeDaFuncao()
+*/
+
+function incentivarQuester () {
+    alert("Você chegou ao JS, continue assim!")
+}
+
+incentivarQuester()
+
+/* Sintaxe da função com parametros
+
+    function nomeDaFuncao (param1, param2) {
+        execucao da funcao
+    }
+
+    nomeDaFuncao(param1, param2)
+*/
+
+function soma(num1, num2) {
+    console.log(num1 + num2)
+}
+
+soma(1,5)
