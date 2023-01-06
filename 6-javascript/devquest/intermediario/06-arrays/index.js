55ca5b755b5e25c75574cf17cbd0afc7f404f211
Raul Rita <raul.vinicius@live.com>
2023-01-06 15:31:32 -0300

    arrays e arrays multidimensionais

---
+++ 6-javascript/devquest/intermediario/06-arrays/index.js
@@ -0,0 +1,24 @@
+/*
+    ARRAYS
+
+    - Definidos por [] podem armazenar até mais de 1 dado e de qualquer tipo, podendo até ser outro array
+    - Para acessar o primeiro dado variável[0]
+*/
+
+// let filmes = ['Vingadores', 'Mulher Maravilha', 'Capitão América']
+
+// console.log(filmes[0])
+
+/*
+    ARRAYS MULTIDIMENSIONAIS
+
+
+*/
+
+let filmes = [
+    ['Vingadores', 4.5, true], 
+    ['Mulher Maravilha', 4, false]
+]
+
+
+console.table(filmes[0][1])
