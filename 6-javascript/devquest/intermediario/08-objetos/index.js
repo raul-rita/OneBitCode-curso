5f41fdb5899c893c48dff73d122527539be57735
Raul Rita <raul.vinicius@live.com>
2023-01-08 11:51:08 -0300

    metodos nos objetos, this e use strict, arrays e objetos em conjunto

---
+++ 6-javascript/devquest/intermediario/08-objetos/index.js
@@ -0,0 +1,10 @@
+let tenis = {
+    cor: 'Azul',
+    tamanho: 40,
+    emEstoque: true
+}
+
+tenis.valor = 300 // adiciona propriedade
+
+console.log(tenis) // mostra o objeto todo
+console.log(tenis.cor) // usando .propriendade acessamos somente aquela propriedade que queremos
---
+++ 6-javascript/devquest/intermediario/11-objetos-e-arrays-em-conjunto/index.js
@@ -0,0 +1,13 @@
+const jogo1 = {
+    nome: 'PES'
+}
+
+let videoGame = {
+    nome: 'X-Box',
+    valor: 3000,
+    jogos: ['Final Fantasy', 'Fallout', 'FIFA']
+}
+
+videoGame.jogos.push(jogo1)
+
+console.log(videoGame)
