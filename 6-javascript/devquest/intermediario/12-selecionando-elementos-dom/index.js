b725049c720645a05c4bd6aac3fe833f59491809
Raul Rita <raul.vinicius@live.com>
2023-01-10 15:46:22 -0300

    selecionando e manipulando elementos no DOM

---
+++ 6-javascript/devquest/intermediario/12-selecionando-elementos-dom/index.js
@@ -0,0 +1,7 @@
+let inputPorClasse = document.getElementsByClassName('tag-de-input');
+
+console.log(inputPorClasse)
+
+let inputPorName = document.querySelectorAll("input[name='email']");
+
+console.log(inputPorName[0])
